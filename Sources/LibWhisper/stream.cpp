f18e478ca2f5c82c2b07deb15aa1b1c273257bc2
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-14 01:01:46 -0400

    wip

--- Sources/LibWhisper/stream.cpp
+++ Sources/LibWhisper/stream.cpp
@@ -46,11 +46,18 @@ struct stream_params stream_default_params() {
 //        /* .model         =*/ "models/ggml-base.en.bin"
 
         /* .n_threads     =*/ std::min(4, (int32_t) std::thread::hardware_concurrency()),
-        /* .step_ms       =*/ 500,
-        /* .length_ms     =*/ 15000,
+//        /* .step_ms       =*/ 500,
+//        /* .length_ms     =*/ 15000,
+//        /* .keep_ms       =*/ 200,
+//        /* .capture_id    =*/ -1,
+//        /* .max_tokens    =*/ 48,
+//        /* .audio_ctx     =*/ 0,
+        
+        /* .step_ms       =*/ 3000,
+        /* .length_ms     =*/ 10000,
         /* .keep_ms       =*/ 200,
         /* .capture_id    =*/ -1,
-        /* .max_tokens    =*/ 48,
+        /* .max_tokens    =*/ 32,
         /* .audio_ctx     =*/ 0,
         
         /* .vad_thold     =*/ 0.6f,
