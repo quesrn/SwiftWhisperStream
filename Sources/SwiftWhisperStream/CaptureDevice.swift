060c4c2502a54a182688cae24abb8a23fc852425
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-14 19:25:51 -0400

    wip

--- Sources/SwiftWhisperStream/CaptureDevice.swift
+++ Sources/SwiftWhisperStream/CaptureDevice.swift
@@ -1,6 +1,7 @@
 import LibWhisper
 //import SDL2
 import SDL
+import CoreAudio
 
 public enum CaptureDeviceError: Error {
     case sdlErrorCode(Int32)
@@ -9,6 +10,64 @@ public enum CaptureDeviceError: Error {
 public struct CaptureDevice: Identifiable {
     public let id: Int32
     public let name: String
+    
+    public func audioDeviceID() -> AudioDeviceID {
+        var audioDeviceID: AudioDeviceID = AudioDeviceID(kAudioDeviceUnknown)
+        
+        var propertyAddress = AudioObjectPropertyAddress(
+            mSelector: kAudioHardwarePropertyDevices,
+            mScope: kAudioObjectPropertyScopeGlobal,
+            mElement: kAudioObjectPropertyElementMain)
+        
+        var deviceList: [AudioDeviceID] = []
+        var dataSize: UInt32 = 0
+        
+        // Get the list of available audio devices
+        let err = AudioObjectGetPropertyDataSize(AudioObjectID(kAudioObjectSystemObject), &propertyAddress, 0, nil, &dataSize)
+        if err != noErr {
+            NSLog("AudioObjectGetPropertyDataSize failed with error (\(err))")
+        }
+        
+        let deviceCount = Int(dataSize) / MemoryLayout<AudioDeviceID>.size
+        deviceList = [AudioDeviceID](repeating: AudioDeviceID(kAudioDeviceUnknown), count: deviceCount)
+        
+        let listErr = AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject), &propertyAddress, 0, nil, &dataSize, &deviceList)
+        if listErr != noErr {
+            NSLog("AudioObjectGetPropertyData failed with error (\(listErr))")
+        }
+        
+        // Find the matching AudioDeviceID based on the device name
+        for deviceID in deviceList {
+            var namePropertyAddress = AudioObjectPropertyAddress(
+                mSelector: kAudioDevicePropertyDeviceName,
+                mScope: kAudioObjectPropertyScopeGlobal,
+                mElement: kAudioObjectPropertyElementMain
+            )
+            
+            var nameSize: UInt32 = 0
+            var deviceName = ""
+            
+            let nameErr = AudioObjectGetPropertyDataSize(deviceID, &namePropertyAddress, 0, nil, &nameSize)
+            if nameErr != noErr {
+                NSLog("AudioObjectGetPropertyDataSize for device name failed with error (\(nameErr))")
+            }
+            
+            var nameData = [CChar](repeating: 0, count: Int(nameSize))
+            let nameDataErr = AudioObjectGetPropertyData(deviceID, &namePropertyAddress, 0, nil, &nameSize, &nameData)
+            if nameDataErr != noErr {
+                NSLog("AudioObjectGetPropertyData for device name failed with error (\(nameDataErr))")
+            }
+            
+            deviceName = String(cString: nameData)
+            
+            if name == deviceName {
+                audioDeviceID = deviceID
+                break
+            }
+        }
+        
+        return audioDeviceID
+    }
     
     public init(id: Int32, name: String) {
         self.id = id
