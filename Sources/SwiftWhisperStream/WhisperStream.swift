51e6f589b6a6d9fa0bd82f27d1ac8f5511b97c4a
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-23 01:20:41 -0400

    wip

--- Sources/SwiftWhisperStream/WhisperStream.swift
+++ Sources/SwiftWhisperStream/WhisperStream.swift
@@ -98,26 +98,39 @@ public class WhisperStream: Thread {
                     let chunkSize = Int32(vad.samples)
                     // Calculate the total number of samples in the input data
                     let totalSamples = length / Int32(MemoryLayout<Uint8>.size)
+                    let totalDurationMicroseconds = (Int64(totalSamples) * 1_000_000) / Int64(WHISPER_SAMPLE_RATE)
                     // Initialize a variable to keep track of the current position
                     var currentPosition: Int32 = 0
+                    var t0 = now - totalDurationMicroseconds
                     // Process audio data with a sliding window
-                    while currentPosition + chunkSize <= totalSamples {
+                    while currentPosition * chunkSize <= totalSamples {
                         // Calculate the offset into the audioBuffer
                         let bufferOffset = currentPosition * Int32(MemoryLayout<Uint8>.size)
-                        
                         let bufferPointer = audioBuffer!.advanced(by: Int(bufferOffset)).withMemoryRebound(to: Uint8.self, capacity: Int(chunkSize) * MemoryLayout<Uint8>.size) { ptr in
                             return ptr
                         }
                         
                         // Calculate t1 and t0 in microseconds
-                        let t1: Int64 = now - Int64(bufferOffset) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE)
-                        let t0: Int64 = max(0, t1 - Int64(chunkSize) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE))
+                        // let t0 be now minus the size of the audioBuffer as measured in microseconds, knowing that the SDL audioBuffer data has sample rate WHISPER_SAMPLE_RATE; then add the currentPosition times the sample chunkSize to get the starting timestamp of the chunk of audio
+                        // let t1 be t0 + the duration of this chunk of audio (expected to be 160 samples chunk size, or in other words, 10ms because the sample rate is 16000)
+                        // Calculate t1 in microseconds by adding the duration of the current chunk
+                        let t1 = t0 + (Int64(chunkSize) * 1_000_000) / Int64(WHISPER_SAMPLE_RATE)
+
+                           // Do something with t0 and t1
+
+                        //                        let t0: Int64 = now - ( Int64(chunkSize) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE))
+//                        let t1: Int64 = now - (Int64(bufferOffset) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE))
+//                        let t0: Int64 = max(0, t1 - Int64(chunkSize) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE))
+//                        let t1: Int64 = now - Int64(bufferOffset) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE)
+//                        let t0: Int64 = max(0, t1 - Int64(chunkSize) * 1_000_000 / Int64(WHISPER_SAMPLE_RATE))
 //                        let t1: Int64 = now - Int64(bufferOffset) * 1000 / WHISPER_SAMPLE_RATE
 //                        // Calculate t0 based on t1 and chunk size
 //                        let t0: Int64 = max(0, t1 - Int64(chunkSize) * 1000 / WHISPER_SAMPLE_RATE)
                         
                         vad.callback(t0: t0, t1: t1, audioBuffer: bufferPointer, length: chunkSize * Int32(MemoryLayout<Uint8>.size))
                         currentPosition += chunkSize
+                        // Increment t0 for the next iteration
+                        t0 = t1
                     }
                 }
                 streamContext = ctx
