654f48d53c819f5fc064d01ad03ac822a4e2bc5e
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-06 20:41:46 -0500

    wip

--- Sources/whisper_cpp/ggml-missing.h
+++ Sources/whisper_cpp/ggml-missing.h
@@ -173,19 +173,19 @@
 //
 //
 //
-//#ifdef GGML_SHARED
-//#    if defined(_WIN32) && !defined(__MINGW32__)
-//#        ifdef GGML_BUILD
-//#            define GGML_API __declspec(dllexport)
-//#        else
-//#            define GGML_API __declspec(dllimport)
-//#        endif
-//#    else
-//#        define GGML_API __attribute__ ((visibility ("default")))
-//#    endif
-//#else
-//#    define GGML_API
-//#endif
+#ifdef GGML_SHARED
+#    if defined(_WIN32) && !defined(__MINGW32__)
+#        ifdef GGML_BUILD
+#            define GGML_API __declspec(dllexport)
+#        else
+#            define GGML_API __declspec(dllimport)
+#        endif
+#    else
+#        define GGML_API __attribute__ ((visibility ("default")))
+#    endif
+#else
+#    define GGML_API
+#endif
 //
 //// TODO: support for clang
 //#ifdef __GNUC__
